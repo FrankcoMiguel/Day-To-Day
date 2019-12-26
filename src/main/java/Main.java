de190dc4a9c2b69231691d84f4baa2d1af3d978a
Frank Orozco <frank_miguel29@outlook.com>
2019-12-26 05:41:07 -0900

    Splash added as first layout

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("layout/Splash.fxml"));
+        primaryStage.setScene(new Scene(root, 1200, 800));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.show();
     }
 
