4149a6b922856da9ec17841c099aaf71090ca61d
Frank Orozco <frank_miguel29@outlook.com>
2020-01-02 23:53:08 -0900

    Homepage is able to add a new task and return back

--- src/main/java/Controller/NewTaskController.java
+++ src/main/java/Controller/NewTaskController.java
@@ -4,12 +4,15 @@ import Model.Task;
 import Service.TaskService;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDatePicker;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.time.LocalDate;
@@ -24,14 +27,25 @@ public class NewTaskController implements Initializable {
     protected JFXDatePicker date;
 
     @FXML
+    protected ImageView backButton;
+
+    @FXML
+    protected FontAwesomeIconView closeButton;
+
+    @FXML
     protected JFXButton addTask;
 
     private TaskService taskService;
+    private ControllerHelper controllerHelper;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        ControllerHelper.closeApp(closeButton);
         saveTask();
+        goBack();
+
+
 
     }
 
@@ -56,6 +70,20 @@ public class NewTaskController implements Initializable {
         return taskName.getText().length() < 40;
 
     }
+
+    private void goBack(){
+
+        backButton.setOnMouseClicked(event -> {
+
+            controllerHelper = new ControllerHelper();
+            controllerHelper.switchStage(backButton,"Homepage");
+
+        });
+
+    }
+
+
+
 
 
 }
